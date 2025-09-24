1e789dca663e0326dcacea89652de628c497874d
Eliška Jelinek <elcaj.sp@gmail.com>
2025-09-24 11:56:28 -0700

    setup form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor" // the rest of the data
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -16,12 +12,13 @@ function MyApp() {
     setCharacters(updated);
   };
 
-  return (
+return (
   <div className="container">
     <Table
       characterData={characters}
       removeCharacter={removeOneCharacter}
     />
+    <Form />
   </div>
 );
 }
